efa2e36383aada1e8ee3867955fa7eb3b1fffb6d
dongjun6343 <dongjun6343@naver.com>
2022-04-15 12:08:38 +0900

    회원 기능 테스트

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,7 +1,6 @@
 package jpabook.jpashop;
 
 import org.assertj.core.api.Assertions;
-
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,30 +9,28 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.Assert.*;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MemberRepositoryTest {
 
-    @Autowired MemberRepository memberRepository;
-
-    @Test
-    @Transactional
-    @Rollback(value = false)
-    public void testMember() throws Exception {
-        Member member = new Member();
-        member.setUsername("memberA");
-
-        // ctrl + alt + v
-        Long saveId = memberRepository.save(member);
-
-        Member findMember = memberRepository.find(saveId);
-
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-        Assertions.assertThat(findMember).isEqualTo(member);
-
-        System.out.println("findMember == member = " + (findMember == member));
-    }
+//    @Autowired MemberRepository memberRepository;
+//
+//    @Test
+//    @Transactional
+//    @Rollback(value = false)
+//    public void testMember() throws Exception {
+//        Member member = new Member();
+//        member.setUsername("memberA");
+//
+//        // ctrl + alt + v
+//        Long saveId = memberRepository.save(member);
+//
+//        Member findMember = memberRepository.find(saveId);
+//
+//        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+//        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+//        Assertions.assertThat(findMember).isEqualTo(member);
+//
+//        System.out.println("findMember == member = " + (findMember == member));
+//    }
 }
