bbc1389b9b25b7412bf0bbc1a44a3f9372d13b3e
dongjun6343 <dongjun6343@naver.com>
2022-05-04 21:13:04 +0900

    변경감지 및 병합 설명

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -62,6 +62,19 @@ public class ItemController {
     }
     @PostMapping("/items/{itemId}/edit")
     public String updateItem(@ModelAttribute("form") BookForm form) {
+
+        // DB에서 저장되고 불러온 엔티티 ==> 준영속 엔티티.
+        // 준영속 엔티티 : 영속성 컨텍스트가 관리하지 않는 엔티티 ==> ex) Book객체.
+
+        // JPA가 관리하지 않는다. ==> DB에서 UPDATE하지 않는다.
+        // 준영속 엔티티를 수정하는 2가지 방법
+        // 1) 변경감지 => 추천.
+        // 2) 병합(merge)사용
+
+        // 병합 사용 시 주의 점
+        // 변경감지는 원하는 속성만 골라서 변경할 수  있지만,
+        // 병합은 모든 속성이 변경된다. ==> 병합 시 값이 없으면 null로 업데이트 할 위험이 있다.
+
         Book book = new Book();
         book.setId(form.getId());
         book.setName(form.getName());
