13d2633ecb0d9cf2b871e4824fa05526620b9e60
dongjun6343 <dongjun6343@naver.com>
2022-05-06 13:39:36 +0900

    주문 생성

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -60,30 +60,35 @@ public class ItemController {
         model.addAttribute("form", form);
         return "items/updateItemForm";
     }
+
+    // DB에서 저장되고 불러온 엔티티 ==> 준영속 엔티티.
+    // 준영속 엔티티 : 영속성 컨텍스트가 관리하지 않는 엔티티 ==> ex) Book객체.
+
+    // JPA가 관리하지 않는다. ==> DB에서 UPDATE하지 않는다.
+    // 준영속 엔티티를 수정하는 2가지 방법
+    // 1) 변경감지 => 추천.
+    // 2) 병합(merge)사용
+
+    // 병합 사용 시 주의 점
+    // 변경감지는 원하는 속성만 골라서 변경할 수  있지만,
+    // 병합은 모든 속성이 변경된다. ==> 병합 시 값이 없으면 null로 업데이트 할 위험이 있다.
     @PostMapping("/items/{itemId}/edit")
-    public String updateItem(@ModelAttribute("form") BookForm form) {
+    public String updateItem(@PathVariable Long itemId , @ModelAttribute("form") BookForm form) {
 
-        // DB에서 저장되고 불러온 엔티티 ==> 준영속 엔티티.
-        // 준영속 엔티티 : 영속성 컨텍스트가 관리하지 않는 엔티티 ==> ex) Book객체.
+        // 컨트롤러에서 어설프게 엔터티를 생성하지 마세요.
+        // 변경 전
+//        Book book = new Book();
+//        book.setId(form.getId());
+//        book.setName(form.getName());
+//        book.setPrice(form.getPrice());
+//        book.setStockQuantity(form.getStockQuantity());
+//        book.setAuthor(form.getAuthor());
+//        book.setIsbn(form.getIsbn());
+//
+//        itemService.save(book);
 
-        // JPA가 관리하지 않는다. ==> DB에서 UPDATE하지 않는다.
-        // 준영속 엔티티를 수정하는 2가지 방법
-        // 1) 변경감지 => 추천.
-        // 2) 병합(merge)사용
-
-        // 병합 사용 시 주의 점
-        // 변경감지는 원하는 속성만 골라서 변경할 수  있지만,
-        // 병합은 모든 속성이 변경된다. ==> 병합 시 값이 없으면 null로 업데이트 할 위험이 있다.
-
-        Book book = new Book();
-        book.setId(form.getId());
-        book.setName(form.getName());
-        book.setPrice(form.getPrice());
-        book.setStockQuantity(form.getStockQuantity());
-        book.setAuthor(form.getAuthor());
-        book.setIsbn(form.getIsbn());
-
-        itemService.save(book);
+        // 변경 후
+        itemService.updateItem(itemId, form.getName(), form.getPrice(), form.getStockQuantity());
         return "redirect:/items";
     }
 }
--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -20,6 +20,22 @@ public class ItemService {
         itemRepository.save(item);
     }
 
+
+    // 파라미터가 많으면
+    //  String name, int price, int stockQuantity) ==> UpdateItemDto를 만들어준다.
+    // public void updateItem(Long itemId, UpdateItemDto dto){}
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity){
+        Item findItem = itemRepository.findOne(itemId);
+
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
+        // 위처럼 set을 해주는것보단 메서드를 하나 만들자
+        // findItem.change(name,price,stockQuantity); ==> 다른사람이 소스를 볼때 편함(인수인계 시..)
+
+    }
+
     public List<Item> findItems() {
         return itemRepository.findAll();
     }
