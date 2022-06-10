50ff050cd48f8f1b2f94294da58ffd2b7323619c
dongjun6343 <dongjun6343@naver.com>
2022-06-10 17:24:17 +0900

    회원 조회 api

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -9,9 +9,11 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
- * API 스펙과 Entity가 1 : 1임  (V1)
+ * API 스펙과 Entity가 1 : 1임 => Entity가 변경되면 API 스펙자체가 변경되어비림! (V1)
  *
  * Entity를 파라미터로 받지 말고 외부에 노출하면 안된다. (V2)
  */
@@ -21,6 +23,34 @@ import javax.validation.constraints.NotEmpty;
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1 (){
+        return memberService.findMembers();
+    }
+
+    // object로 한번 감싸준다. =>  유연성 { "data" : [  { "name" : "A" } , { "name" : "B" } , {} ] }
+    @GetMapping("/api/v2/members")
+    public Result memberV2(){
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+        return new Result(collect.size(), collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto{
+        private String name;
+    }
 
     // 회원 등록 api
     // Valid : javax.validation에 대한 validation을 해줌.
