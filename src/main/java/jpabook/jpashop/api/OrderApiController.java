b91734f92c77962999d53c956c7954bd44058251
dongjun6343 <dongjun6343@naver.com>
2022-06-28 22:52:26 +0900

    쿼리 최적화 ( 2번으로 줄임 ) - V5

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -85,6 +85,12 @@ public class OrderApiController {
         return orderQueryRepository.findOrderQueryDtos();
     }
 
+
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> orderV5(){
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
     @Getter
     static class OrderDto {
         private Long orderId;
--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
@@ -22,6 +24,33 @@ public class OrderQueryRepository {
         return result;
     }
 
+    // V4의 단점 : for문을 돈다. --> 쿼리 최적화.
+    public List<OrderQueryDto> findAllByDto_optimization(){
+        List<OrderQueryDto> result = findOrders();
+
+        // result 를 스트림으로 돌려서 ID를 뽑는다.
+        List<Long> orderIds = result.stream()
+                .map(o -> o.getOrderId())
+                .collect(Collectors.toList());
+
+        List<OrderItemQueryDto> orderItems = em.createQuery(
+                        "select new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name,oi.orderPrice, oi.count)" +
+                                " from OrderItem oi" +
+                                " join oi.item i" +
+                                " where oi.order.id in : orderIds",
+                        OrderItemQueryDto.class)
+                .setParameter("orderIds", orderIds)
+                .getResultList();
+
+            // getOrderId를 기준으로 map으로 바뀜.
+        Map<Long, List<OrderItemQueryDto>> orderItemMap = orderItems.stream()
+                .collect(Collectors.groupingBy(orderItemQueryDto -> orderItemQueryDto.getOrderId()));
+
+        // 쿼리 2번으로 최적화.
+        result.forEach(o -> o.setOrderItems(orderItemMap.get(o.getOrderId())));
+
+        return result;
+    }
 
     /**
      * 1:N 관계(컬렉션)를 제외한 나머지를 한번에 조회
